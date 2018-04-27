2e1ca99c823b44d584f7ef64d5520167e8521359
rgathergood <rgathergood@gmail.com>
2018-04-27 10:52:22 +0100

    countries_data created

---
+++ client/src/models/countries_data.js
@@ -0,0 +1,13 @@
+const Request = require('../services/request.js')
+
+const CountriesData = function () {
+  this.url = 'https://restcountries.eu/rest/v2/all';
+}
+
+CountriesData.prototype.getData = function (onComplete) {
+  const request = new Request(this.url);
+  request.get((data) => {
+    this.data = data;
+    onComplete(data);
+  })
+};
