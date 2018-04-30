c48fbc2f7d2426ced3b9e73b5a736a6ff5c9be7e
Bert Overduin <boverduin@gmail.com>
2018-04-30 13:26:46 +0100

    Refactored app.js

--- client/src/app.js
+++ client/src/app.js
@@ -3,6 +3,8 @@ const CountriesData = require('./models/countries_data.js');
 const QuizData = require('./models/quiz_data.js');
 const QuizView = require('./views/quiz_view.js');
 
+let score;
+
 document.addEventListener('DOMContentLoaded', () => {
   const quizStartButton = document.querySelector("#flag-quiz-button");
   const quizContainer = document.querySelector("#quiz-container");
@@ -11,38 +13,31 @@ document.addEventListener('DOMContentLoaded', () => {
   const quizData = new QuizData(countriesData);
   const quizView = new QuizView(quizContainer);
 
-  let score;
-  const incrementScore = function() {
-    score += 1;
-  }
-
-  const renderNewQuestion = function(index) {
-    if (index < quizData.questions.length - 1) {
-      quizView.renderQuestion(
-        quizData.questions[index + 1],
-        () => {
-          renderNewQuestion(index + 1);
-        },
-        incrementScore
-      );
-    }
-    else {
-      quizView.renderResult(score);
-    }
-  }
-
   countriesData.getData((countries) => {
     quizStartButton.addEventListener('click', () =>  {
       quizData.generateQuiz();
       score = 0;
-      // let index = 0;
-      renderNewQuestion(-1);
-      // quizView.renderQuestion(quizData.questions[0], () => {
-      //   renderNewQuestion(index);
-      //   },
-      //   incrementScore
-      // );
+      renderNewQuestion(-1, quizData, quizView);
     });
   });
 
 });
+
+const incrementScore = function() {
+  score += 1;
+}
+
+const renderNewQuestion = function(index, quizData, quizView) {
+  if (index < quizData.questions.length - 1) {
+    quizView.renderQuestion(
+      quizData.questions[index + 1],
+      () => {
+        renderNewQuestion(index + 1, quizData, quizView);
+      },
+      incrementScore
+    );
+  }
+  else {
+    quizView.renderResult(score);
+  }
+}
