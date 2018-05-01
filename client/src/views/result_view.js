9f02f69df52323a8e3c25f20c2430292f2e1fab3
rgathergood <rgathergood@gmail.com>
2018-05-01 10:35:13 +0100

    Table inserted

--- client/src/views/result_view.js
+++ client/src/views/result_view.js
@@ -9,6 +9,15 @@ ResultView.prototype.renderResult = function (result) {
   text.classList.add('result-text');
   text.textContent = `Congratulations, ${result.name}! You got ${result.score} out of 10!`;
   this.container.appendChild(text);
+
+  const table = document.createElement('table');
+  table.classList.add('results-table');
+  this.container.appendChild(table);
+  const row = table.insertRow(0);
+  const nameHeader = row.insertCell(0);
+  const scoreHeader = row.insertCell(1);
+  nameHeader.innerHTML = 'Name';
+  scoreHeader.innerHTML = 'Score';
 };
 
 module.exports = ResultView;
