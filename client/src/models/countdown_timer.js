d502e44b2d21e27ebf63cf71c642c4f8426afdc0
drp33 <dan@mechanochemistry.org>
2018-05-01 11:22:04 +0100

    timer counts down and stops at zero, pretty formatting

--- client/src/models/countdown_timer.js
+++ client/src/models/countdown_timer.js
@@ -1,17 +1,30 @@
 const CountdownTimer = function(duration) {
-
   this.duration = duration;
 }
 
 CountdownTimer.prototype.start = function() {
   // todo: set display = duration
   this.startTime = Date.now();
-  this.interval = setInterval(this.printTimeLeft, 1000);
+  this.interval = setInterval(() => this.printTimeLeft(), 1000);
 }
 
 CountdownTimer.prototype.printTimeLeft = function() {
-  const secondsElapsed = Math.floor((Date.now() - this.startTime)/1000);
-  console.log(secondsElapsed);
+  const secondsExpired = Math.floor((Date.now() - this.startTime)/1000);
+  const totalSecondsLeft = this.duration - secondsExpired;
+
+  let minutes = Math.floor(totalSecondsLeft/60);
+  let seconds = Math.floor(totalSecondsLeft % 60);
+
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  seconds = seconds < 10 ? '0' + seconds : seconds;
+
+  const timeString = minutes + ':' + seconds;
+  console.log(timeString);
+
+  if (totalSecondsLeft <= 0) {
+    clearInterval(this.interval);
+  }
+
 }
 
 module.exports = CountdownTimer;
