be203295b9e5a595bf3bd1cbb48e3a00d62f2688
Bert Overduin <boverduin@gmail.com>
2018-05-01 10:05:59 +0100

    Added result view

--- client/src/app.js
+++ client/src/app.js
@@ -3,6 +3,7 @@ const CountriesData = require('./models/countries_data.js');
 const QuizData = require('./models/quiz_data.js');
 const QuizView = require('./views/quiz_view.js');
 const StartView = require('./views/start_view.js');
+const ResultView = require('./views/result_view.js');
 
 const leaderboardRequest = new Request('./db/leaderboard');
 let result = {};
@@ -15,6 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const quizData = new QuizData(countriesData);
   const quizView = new QuizView(quizContainer);
   const startView = new StartView(quizContainer);
+  const resultView = new ResultView(quizContainer);
 
   countriesData.getData(() => {
     newQuizButton.addEventListener('click', () =>  {
@@ -24,7 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
           quizData.generateQuiz();
           result.score = 0;
           result.name = input.value;
-          renderNewQuestion(-1, quizData, quizView);
+          renderNewQuestion(-1, quizData, quizView, resultView);
         });
       });
     });
@@ -35,18 +37,18 @@ const incrementScore = function() {
   result.score += 1;
 }
 
-const renderNewQuestion = function(index, quizData, quizView) {
+const renderNewQuestion = function(index, quizData, quizView, resultView) {
   if (index < quizData.questions.length - 1) {
     quizView.renderQuestion(
       quizData.questions[index + 1],
       () => {
-        renderNewQuestion(index + 1, quizData, quizView);
+        renderNewQuestion(index + 1, quizData, quizView, resultView);
       },
       incrementScore
     );
   }
   else {
-    quizView.renderResult(result);
+    resultView.renderResult(result);
     leaderboardRequest.post(result, () => {console.log('Successfully wrote to db')});
   }
 }
