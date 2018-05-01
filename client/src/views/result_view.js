1419e286f9abbe425d465b11c4fdab8b5c0db2ec
Bert Overduin <boverduin@gmail.com>
2018-05-01 10:47:54 +0100

    Added row to results table

--- client/src/views/result_view.js
+++ client/src/views/result_view.js
@@ -13,11 +13,19 @@ ResultView.prototype.renderResult = function (result) {
   const table = document.createElement('table');
   table.classList.add('results-table');
   this.container.appendChild(table);
+
   const row = table.insertRow(0);
   const nameHeader = row.insertCell(0);
   const scoreHeader = row.insertCell(1);
   nameHeader.innerHTML = 'Name';
   scoreHeader.innerHTML = 'Score';
+
+  const resultsRow = table.insertRow(1);
+  const playerName = resultsRow.insertCell(0);
+  const playerScore = resultsRow.insertCell(1);
+  playerName.innerHTML = `${result.name}`;
+  playerScore.innerHTML = `${result.score}`;
+
 };
 
 module.exports = ResultView;
