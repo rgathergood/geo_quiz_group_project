0a475a1fdeb4295578534a0f0d1a3dbb25061ad3
drp33 <dan@mechanochemistry.org>
2018-05-02 14:47:55 +0100

    added mp3 player to main page, created audioView

---
+++ client/src/views/audio_view.js
@@ -0,0 +1,10 @@
+const AudioView = function(container, audioData) {
+  this.container = container;
+  this.audioData = audioData;
+}
+
+AudioView.prototype.render = function () {
+  
+};
+
+module.exports = AudioView;
