5a20ecf47c3f3ccdda4b6bced84c18a5408f1b31
drp33 <dan@mechanochemistry.org>
2018-05-02 16:46:15 +0100

    show name of country when playing anthem

--- client/src/views/audio_view.js
+++ client/src/views/audio_view.js
@@ -9,13 +9,22 @@ AudioView.prototype.render = function () {
   this.audioPlayer.innerHTML = '';
   this.audioPlayer.src = this.audioData.data[this.index].anthem;
   this.audioPlayer.addEventListener('ended', () => {this.nextTrack()});
+  this.refreshDisplay();
 };
 
 AudioView.prototype.nextTrack = function() {
   this.index++;
   if (this.index === this.audioData.length) this.index = 0;
   this.audioPlayer.src = this.audioData.data[this.index].anthem;
-  console.dir(this.audioData.data[this.index].anthem);
+  this.refreshDisplay();
 }
+
+AudioView.prototype.refreshDisplay = function () {
+  const audioDisplay = document.querySelector('#audio-display');
+  audioDisplay.innerHTML = '';
+  const h4 = document.createElement('h4');
+  h4.textContent = this.audioData.data[this.index].name;
+  audioDisplay.appendChild(h4);
+};
 
 module.exports = AudioView;
